b7754d6b5b68e242c752adb148d22c14e928e929
thienanh96 <thienanhnguyen06@gmail.com>
2018-12-21 21:05:10 +0700

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/web-app-chat-client'));
 app.listen(process.env.PORT || 8000);
 
-app.get('/*',function(req,res){
+app.get('*',function(req,res){
     res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
